c01232d1b8c9f4ee6690b5cbdf847586c1ea490b
davistdaniel <davisthomasdaniel@gmail.com>
2025-12-20 22:55:29 +0100

    Added start_workflow

--- src/chempleter/__init__.py
+++ src/chempleter/__init__.py
@@ -1,1 +1,15 @@
 # chempleter
+
+from pathlib import Path
+
+def start_workflow(experiment_name,working_dir=None):
+
+    if not working_dir:
+        working_dir = Path().cwd() / experiment_name
+    else:
+        working_dir = Path(working_dir) /experiment_name
+    
+    # make dir
+    working_dir.mkdir(parents=True, exist_ok=True)
+
+    return working_dir
