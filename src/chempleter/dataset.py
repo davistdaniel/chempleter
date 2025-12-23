95585730e099e0739b9fb9d45e4446accb6a0a47
davistdaniel <davisthomasdaniel@gmail.com>
2025-12-23 16:13:21 +0100

    Added dataloader def

--- src/chempleter/dataset.py
+++ src/chempleter/dataset.py
@@ -3,6 +3,8 @@ import torch
 import selfies as sf
 import pandas as pd
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 class ChempleterDataset(Dataset):
     def __init__(self, selfies_file,stoi_file):
@@ -21,3 +23,19 @@ class ChempleterDataset(Dataset):
         symbols_molecule = ["[START]"]+list(sf.split_selfies(molecule))+["[END]"]
         integer_molecule = [self.selfies_to_integer[symbol] for symbol in symbols_molecule]
         return torch.tensor(integer_molecule,dtype=torch.long)
+
+# def collate_fn(batch):
+#     return pad_sequence(batch,batch_first=True)
+
+def collate_fn(batch):
+
+    tensor_lengths  = torch.tensor([len(x) for x in batch])
+    tensor_lengths, sorted_idx = tensor_lengths.sort(descending=True)
+    batch = [batch[i] for i in sorted_idx]
+    
+    padded_batch = pad_sequence(batch, batch_first=True,padding_value=0)
+
+    return padded_batch, tensor_lengths
+
+def get_dataloader(dataset,batch_size=64,shuffle=True,collate_fn=collate_fn):
+    return DataLoader(dataset,batch_size=batch_size,shuffle=shuffle,collate_fn=collate_fn)
