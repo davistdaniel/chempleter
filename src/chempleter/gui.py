5a64bdacd79fabd5bf32dc222963c6770d4edf41
davistdaniel <davisthomasdaniel@gmail.com>
2026-01-02 15:53:20 +0100

    feat: added bridge option in gui

--- src/chempleter/gui.py
+++ src/chempleter/gui.py
@@ -3,7 +3,7 @@ import base64
 import json
 import torch
 from nicegui import ui
-from chempleter.inference import handle_prompt, extend, evolve
+from chempleter.inference import handle_prompt, extend, evolve, bridge
 from chempleter.model import ChempleterModel
 from chempleter.descriptors import calculate_descriptors
 from pathlib import Path
@@ -26,60 +26,94 @@ def build_chempleter_ui():
         else "cpu"
     )
 
-    default_stoi_file = Path(resources.files("chempleter.data").joinpath("stoi.json"))
-    default_itos_file = Path(resources.files("chempleter.data").joinpath("itos.json"))
-    default_checkpoint_file = Path(
-        resources.files("chempleter.data").joinpath("model.pt")
-    )
-
-    with open(default_stoi_file) as f:
-        stoi = json.load(f)
-
-    model = ChempleterModel(vocab_size=len(stoi))
-    checkpoint = torch.load(
-        default_checkpoint_file, map_location=device, weights_only=True
-    )
-    model.load_state_dict(checkpoint["model_state_dict"])
-
-    def _validate_smiles(smiles):
+    def _validate_smiles(smiles,frag1_smiles=None,frag2_smiles=None):
         try:
             prompt = handle_prompt(
                 smiles,
                 selfies=None,
                 stoi=None,
                 alter_prompt=alter_prompt_checkbox.value,
+                frag1_smiles=frag1_smiles,
+                frag2_smiles=frag2_smiles
             )
             return True, prompt
         except Exception as e:
             print(e)
             return False, ""
+        
+    def _draw_if_bridge(generated_molecule):
+
+        frag1_smiles = smiles_input.value
+        frag2_smiles = smiles_input2.value
+
+        frag1_structure = MolFromSmiles(frag1_smiles)
+        frag2_structure = MolFromSmiles(frag2_smiles)
+
+        if frag1_structure is None or frag2_structure is None:
+            return Draw.MolToImage(generated_molecule, size=(300, 300))
+        
+
+        frag_atoms = set()
+
+        matches1 = generated_molecule.GetSubstructMatches(frag1_structure)
+        matches2 = generated_molecule.GetSubstructMatches(frag2_structure)
+
+        for match in matches1 + matches2:
+            for atom_idx in match:
+                frag_atoms.add(atom_idx)
+
+        if not frag_atoms:
+            return Draw.MolToImage(generated_molecule, size=(300, 300))
+        
+        bridge_atoms = [
+        atom.GetIdx()
+        for atom in generated_molecule.GetAtoms()
+        if atom.GetIdx() not in frag_atoms
+        ]
+
+        bridge_bonds = []
+        for bond in generated_molecule.GetBonds():
+            a1 = bond.GetBeginAtomIdx()
+            a2 = bond.GetEndAtomIdx()
+            if a1 not in frag_atoms and a2 not in frag_atoms:
+                bridge_bonds.append(bond.GetIdx())
+
+        img = Draw.MolToImage(
+                generated_molecule,
+                size=(300, 300),
+                highlightAtoms=bridge_atoms,
+                highlightBonds=bridge_bonds,
+            )
+        
+        return img
+
 
     def _draw_if_extend(generated_molecule):
 
         # try highlighting input molecule
         input_molecule_structure = MolFromSmiles(smiles_input.value)
-        if input_molecule_structure is not None:
-            match = generated_molecule.GetSubstructMatch(input_molecule_structure)
-            highlight_atoms = list(match) if match else []
-            highlight_bonds = []
-            for bond in generated_molecule.GetBonds():
-                a1 = bond.GetBeginAtomIdx()
-                a2 = bond.GetEndAtomIdx()
-                if a1 in highlight_atoms and a2 in highlight_atoms:
-                    highlight_bonds.append(bond.GetIdx())
-
-            img = Draw.MolToImage(
-                generated_molecule,
-                size=(300, 300),
-                highlightAtoms=highlight_atoms,
-                highlightBonds=highlight_bonds,
-                highlightAtomColors={i: (1.0, 0.0, 0.0) for i in highlight_atoms},
-                highlightBondColors={i: (1.0, 0.0, 0.0) for i in highlight_bonds},
-            )
-
-        else:
-            img = Draw.MolToImage(generated_molecule, size=(300, 300))
+        if input_molecule_structure is None:
+            return Draw.MolToImage(generated_molecule, size=(300, 300))
         
+        match = generated_molecule.GetSubstructMatch(input_molecule_structure)
+        if match is None:
+            return Draw.MolToImage(generated_molecule, size=(300, 300))
+        
+        highlight_atoms = list(match) if match else []
+        highlight_bonds = []
+        for bond in generated_molecule.GetBonds():
+            a1 = bond.GetBeginAtomIdx()
+            a2 = bond.GetEndAtomIdx()
+            if a1 in highlight_atoms and a2 in highlight_atoms:
+                highlight_bonds.append(bond.GetIdx())
+
+        img = Draw.MolToImage(
+            generated_molecule,
+            size=(300, 300),
+            highlightAtoms=highlight_atoms,
+            highlightBonds=highlight_bonds,
+        )
+    
         return img
 
     def _draw_if_evolve(generated_molecule):
@@ -141,9 +175,17 @@ def build_chempleter_ui():
         return img
 
     def show_generated_molecule():
-        if _validate_smiles(smiles=smiles_input.value)[0] is False:
-            ui.notify("Error parsing input SMILES", type="negative")
-            return
+        if generation_type_radio.value!="Bridge":
+            if _validate_smiles(smiles=smiles_input.value)[0] is False:
+                ui.notify("Error parsing input SMILES", type="negative")
+                return
+        else:
+            if smiles_input.value=="" or smiles_input2.value=="":
+                ui.notify("For bridging, two input smiles must be provided.", type="negative")
+                return
+            if _validate_smiles(smiles=smiles_input.value)[0] is False or _validate_smiles(smiles=smiles_input2.value)[0] is False:
+                ui.notify("Error parsing input SMILES", type="negative")
+                return
 
         smiles_input.disable()
         generate_button.set_enabled(False)
@@ -156,19 +198,34 @@ def build_chempleter_ui():
             "greedy" if sampling_radio.value == "Most probable" else "top_k_temperature"
         )
 
-        gen_func = extend if generation_type_radio.value=="Extend" else evolve
-        # generate
-        generated_molecule, generated_smiles, _ = gen_func(
-            model=model,
-            stoi_file=default_stoi_file,
-            itos_file=default_itos_file,
-            smiles=smiles_input.value,
-            min_len=min_len,
-            max_len=max_len,
-            temperature=temperature_input.value,
-            alter_prompt=alter_prompt_checkbox.value,
-            next_atom_criteria=next_atom_criteria,
-        )
+        gen_func_dict = {"Extend":extend, "Evolve":evolve, "Bridge":bridge}
+
+        gen_func = gen_func_dict[generation_type_radio.value]
+
+        if generation_type_radio.value!="Bridge":
+            # generate
+            generated_molecule, generated_smiles, _ = gen_func(
+                model=None,
+                stoi_file=None,
+                itos_file=None,
+                smiles=smiles_input.value,
+                min_len=min_len,
+                max_len=max_len,
+                temperature=temperature_input.value,
+                alter_prompt=alter_prompt_checkbox.value,
+                next_atom_criteria=next_atom_criteria,
+            )
+
+        else:
+            generated_molecule, generated_smiles, _ = gen_func(
+                frag1_smiles=smiles_input.value,
+                frag2_smiles=smiles_input2.value,
+                model=None,
+                stoi_file=None,
+                itos_file=None,
+                temperature=temperature_input.value,
+                next_atom_criteria=next_atom_criteria,
+            )
 
         # check if same
         if isinstance(generated_smiles,list):
@@ -187,6 +244,9 @@ def build_chempleter_ui():
                     
         if generation_type_radio.value=="Extend":
             img = _draw_if_extend(generated_molecule)
+            molecule_image.style("width: 300px")
+        elif generation_type_radio.value=="Bridge":
+            img = _draw_if_bridge(generated_molecule)
             molecule_image.style("width: 300px")
         else:
             img = _draw_if_evolve(generated_molecule)
@@ -212,6 +272,25 @@ def build_chempleter_ui():
         smiles_input.enable()
         generate_button.set_enabled(True)
 
+    def handle_generation_type_change():
+        if generation_type_radio.value == "Bridge":
+            info_label.set_text("Two molecular fragments will be bridged. Bridge will be highlighted in the output.")
+            smiles_input2.set_visibility(True)
+            length_slider.disable()
+            alter_prompt_checkbox.disable()
+            temperature_input.props("max=2")
+        elif generation_type_radio.value == "Extend":
+            info_label.set_text("Input molecular fragment will be extended. Input molecular fragment will be highlighted in the output.")
+            length_slider.enable()
+            alter_prompt_checkbox.enable()
+            smiles_input2.set_visibility(False)
+            temperature_input.props("max=5")
+        elif generation_type_radio.value=="Evolve":
+            info_label.set_text("Input molecular fragment will be evolved. New fragments added at each evolution step will be highlighted in the output.")
+            length_slider.enable()
+            alter_prompt_checkbox.disable()
+            smiles_input2.set_visibility(False)
+            temperature_input.props("max=5")
 
     logo_path = Path(resources.files("chempleter.data").joinpath("chempleter_logo.png"))
     ui.page_title("Chempleter")
@@ -226,9 +305,17 @@ def build_chempleter_ui():
                     "Enter SMILES",
                     placeholder="c1ccccc1",
                     validation=lambda value: "Invalid SMILES"
-                    if _validate_smiles(value)[0] is False
+                    if _validate_smiles(smiles=value)[0] is False
                     else None,
                 )
+                smiles_input2 = ui.input(
+                    "Enter SMILES 2",
+                    placeholder="c1ccccc1",
+                    validation=lambda value: "Invalid SMILES"
+                    if _validate_smiles(smiles=value)[0] is False
+                    else None,
+                )
+                smiles_input2.set_visibility(False)
 
             with ui.row(wrap=True).classes("w-full justify-center items-center"):
                 ui.separator()
@@ -245,8 +332,8 @@ def build_chempleter_ui():
             with ui.row(wrap=False):
                 ui.chip("Generation type: ", color="white")
                 generation_type_radio = ui.radio(
-                    ["Extend", "Evolve"], value="Extend"
-                ).props("inline")
+                    ["Extend", "Evolve", "Bridge"], value="Extend",
+                on_change=handle_generation_type_change).props("inline")
             with ui.row(wrap=False).classes("w-128"):
                 ui.chip("Molecule size: ", color="white")
                 ui.chip("Smaller")
@@ -256,6 +343,8 @@ def build_chempleter_ui():
         with ui.row():
             ui.label("Processed prompt :")
             ui.label().bind_text_from(smiles_input, "value", backward=_validate_smiles)
+        with ui.row():
+            info_label = ui.label("Click on Generate to start generating molecules.")
 
         with ui.row().classes("w-128 justify-center"):
             generate_button = ui.button("Generate", on_click=show_generated_molecule)
